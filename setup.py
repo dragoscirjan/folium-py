615b23625b2d91ec590ac9223fde09a98f11012b
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2019-06-09 00:44:01 +0300

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Folium',
+      version='0.0.1',
+      description='Folium Services Interfaces',
+      author='Dragos Cirjan',
+      author_email='dragos.cirjan@gmail.com',
+      url='https://github.com/dragoscirjan/folium-py',
+      packages=['folium'],
+     )
